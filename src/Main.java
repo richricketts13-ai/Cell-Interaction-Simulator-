f671c9cf501308de158db9acacaaaf016078a8b6
RichieCodes <rich.ricketts13@gmail.com>
2026-04-30 18:08:50 -0400

    Version 2 - multi-cell simulation with dynamic interactions

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,77 @@
 import java.util.Random;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+
 
 
 public class Main {
     public static void main(String[] args) {
 
+        Random RAND = new Random();
+
+
         try {
 
         FileWriter writer = new FileWriter("simulation_log.txt");
 
-        Cell cellA = createRandomCell();
+       List<Cell> cells = new ArrayList<>();
 
-        Cell cellB = createRandomCell();
+       for (int i = 0; i < 3; ++i) {
+              cells.add(createRandomCell());
+       }
+
 
         int round = 1;
 
-        while (cellA.isAlive() && cellB.isAlive()) {
+        while (cells.size() > 1) {
             System.out.println("Round " + round);
             writer.write("Round " + round + "\n");
             round++;
 
-            performAction(cellA, cellB);
+            // 1. Actions
+            for (Cell cell : cells) {
+                if (!cell.isAlive()) continue;
 
-            if (cellB.isAlive()) {
-                performAction(cellB, cellA);
-            }
-            System.out.println();
-            System.out.println(cellA.getName() + " integrity: " + cellA.getCellHealth());
-            writer.write(cellA.getName() + " integrity: " + cellA.getCellHealth() + "\n");
+                 Cell targetCell = cells.get(RAND.nextInt(cells.size()));
 
-            System.out.println(cellB.getName() + " integrity: " + cellB.getCellHealth());
-            writer.write(cellB.getName() + " integrity: " + cellB.getCellHealth() + "\n");
+                  while (targetCell == cell) {
+
+                       targetCell = cells.get(RAND.nextInt(cells.size()));
+                    }
+
+        performAction(cell, targetCell, RAND);
+    }
+
+    // 2. Log deaths
+    for (Cell cell : cells) {
+        if (!cell.isAlive()) {
+
+            System.out.println(cell.getName() + " has died.");
+            writer.write(cell.getName() + " has died.\n");
         }
-        if (cellA.isAlive()) {
-            System.out.println(cellA.getName() + " wins!");
-            writer.write(cellA.getName() + " wins!\n");
+    }
 
-        } else if (cellB.isAlive()) {
-            System.out.println(cellB.getName() + " wins!");
-            writer.write(cellB.getName() + " wins!\n");
-            
-        } else {
-            System.out.println("It's a draw!");
-            writer.write("It's a draw!\n");
+    // 3. Remove dead cells
+    for (int i = cells.size() - 1; i >= 0; i--) {
+        if (!cells.get(i).isAlive()) {
+            cells.remove(i);
         }
+    }
+
+    for (Cell cell : cells) {
+        System.out.println(cell.getName() + " has " + cell.getCellHealth() + " integrity");
+        writer.write(cell.getName() + " has " + cell.getCellHealth() + " integrity\n");
+    }
+    System.out.println();
+    writer.write("\n");
+}
+
+if (cells.size() == 1) {
+    System.out.println(cells.get(0).getName() + " is the last cell standing!");
+    writer.write(cells.get(0).getName() + " is the last cell standing!\n");
+}
         writer.close();
             
         } catch (IOException e) {
@@ -78,7 +105,7 @@ public class Main {
 
 
 
-    public static void performAction(Cell cell, Cell targetCell) {
+    public static void performAction(Cell cell, Cell targetCell, Random RAND) {
 
     int maxHealth = cell.getMaxCellHealth();
 
@@ -103,7 +130,7 @@ public class Main {
             }
         }
     } else {
-        switch (new Random().nextInt(3) + 1) {
+        switch (RAND.nextInt(3) + 1) {
             case 1 -> cell.interact(targetCell);
             case 2 -> cell.adapt();
             case 3 -> {
